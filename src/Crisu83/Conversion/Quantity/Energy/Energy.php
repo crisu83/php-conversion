4a671a8c2a80ba6b779c01343775f698175c81fb
Ujjwal Ojha <bring2uo@gmail.com>
2014-06-15 08:51:49 +0545

    Created new quantity energy

---
+++ src/Crisu83/Conversion/Quantity/Energy/Energy.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * This file is part of Conversion.
+ *
+ * (c) 2013 Christoffer Niska
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Crisu83\Conversion\Quantity\Energy;
+
+use Crisu83\Conversion\Quantity\Quantity;
+
+/**
+ * Class Power
+ * @package Crisu83\Conversion\Quantity\Power
+ */
+class Energy extends Quantity
+{
+    /**
+     * @var string native unit name
+     */
+    protected static $native = Unit::JOULE;
+
+    /**
+     * @var array conversion map (unit => native unit)
+     */
+    protected static $conversionMap = [
+        Unit::JOULE     => 1,
+        Unit::CALORIE   => 4.184,
+        Unit::KILOWATT_HOUR  => 3.6e+6,
+    ];
+}
