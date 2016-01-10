4352b56b744a8ca8c6544ba3ff176f6c624c2ba5
Allan Sun <allan.sun@bricre.com>
2016-01-10 13:14:08 +0000

    Added Cubic MilliMetre and Cubit CentiMetre Conversion

--- src/Crisu83/Conversion/Quantity/Volume/Volume.php
+++ src/Crisu83/Conversion/Quantity/Volume/Volume.php
@@ -14,6 +14,7 @@ use Crisu83\Conversion\Quantity\Quantity;
 
 /**
  * Class Volume
+ *
  * @package Crisu83\Conversion\Quantity\Volume
  */
 class Volume extends Quantity
@@ -27,23 +28,25 @@ class Volume extends Quantity
      * @var array conversion map (unit => native unit)
      */
     protected static $conversionMap = array(
-        Unit::MILLILITRE    => 1e-6,
-        Unit::LITRE         => 0.001,
-        Unit::CUBIC_METRE   => 1,
-        Unit::GALLON        => 0.00454609,
-        Unit::QUART         => 0.00113652,
-        Unit::PINT          => 0.000568261,
-        Unit::OUNCE         => 2.8413e-5,
-        Unit::TABLESPOON    => 1.7758e-5,
-        Unit::TEASPOON      => 5.9194e-6,
-        Unit::US_GALLON     => 0.00378541,
-        Unit::US_QUART      => 0.000946353,
-        Unit::US_PINT       => 0.000473176,
-        Unit::US_CUP        => 0.000236588,
-        Unit::US_OUNCE      => 2.9574e-5,
-        Unit::US_TABLESPOON => 1.4787e-5,
-        Unit::US_TEASPOON   => 4.9289e-6,
-        Unit::CUBIC_INCH    => 1.6387e-5,
-        Unit::CUBIC_FOOT    => 0.0283168,
+        Unit::MILLILITRE       => 1e-6,
+        Unit::CUBIC_MILLIMETRE => 1e-9,
+        Unit::CUBIC_CENTIMETRE => 1e-6,
+        Unit::LITRE            => 0.001,
+        Unit::CUBIC_METRE      => 1,
+        Unit::GALLON           => 0.00454609,
+        Unit::QUART            => 0.00113652,
+        Unit::PINT             => 0.000568261,
+        Unit::OUNCE            => 2.8413e-5,
+        Unit::TABLESPOON       => 1.7758e-5,
+        Unit::TEASPOON         => 5.9194e-6,
+        Unit::US_GALLON        => 0.00378541,
+        Unit::US_QUART         => 0.000946353,
+        Unit::US_PINT          => 0.000473176,
+        Unit::US_CUP           => 0.000236588,
+        Unit::US_OUNCE         => 2.9574e-5,
+        Unit::US_TABLESPOON    => 1.4787e-5,
+        Unit::US_TEASPOON      => 4.9289e-6,
+        Unit::CUBIC_INCH       => 1.6387e-5,
+        Unit::CUBIC_FOOT       => 0.0283168,
     );
 }
